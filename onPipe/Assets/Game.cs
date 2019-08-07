4d37a9f1052de519af324b89c6b7c6397665c871
kamal johnson <kamaljohnson12345@gmail.com>
2019-08-08 00:05:05 +0530

    creation moving deletion of random size and scal pipes

---
+++ onPipe/Assets/Game.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public float pipeSpeed;
+    
+    public Transform pipeHolder;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        pipeHolder.transform.localPosition += new Vector3(0, -Time.deltaTime * pipeSpeed, 0);
+    }
+}
--- onPipe/Assets/PipeCreator.cs
+++ onPipe/Assets/PipeCreator.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -11,12 +12,14 @@ public enum PipeType
 
 public class PipeCreator : MonoBehaviour
 {
-    public float pipeSpeed;
+    private float _pipeSpeed;
     
     public List<GameObject> generalPipe;
     public List<GameObject> obstreclePipe;
 
-    public Transform pipeHolder;
+    private Transform pipeHolder;
+    public Transform pipeCreationSensor;
+    public Transform pipeDeletionSensor;
 
     public Transform createLocation;
 
@@ -25,28 +28,60 @@ public class PipeCreator : MonoBehaviour
     private List<GameObject> _bufferListOfPipes;
 
     private bool _createPipe;
-    private float _pipeCreationTimer;
-    
+
+    public List<float> pipeSizes = new List<float>();
+
     void Start()
     {
+        _pipeSpeed = gameObject.GetComponent<Game>().pipeSpeed;
+        pipeHolder = gameObject.GetComponent<Game>().pipeHolder;
+        
         _bufferListOfPipes = new List<GameObject>();
         _currentGeneralPipeIndex = 0;
         _createPipe = true;
-        _pipeCreationTimer = 1 / pipeSpeed;
     }
 
     void Update()
     {
-        if (_pipeCreationTimer >= 1 / pipeSpeed)
+
+        if (checkPipeCreationSensor())
+        {
+            _createPipe = true;
+        }
+        
+        if (_createPipe)
         {
             CreatePipe(PipeType.General);
             _createPipe = false;
-            _pipeCreationTimer = 0;
         }
-
-        _pipeCreationTimer += Time.deltaTime;
+        
+        checkPipeDeletionSensor();
     }
 
+    public void checkPipeDeletionSensor()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(pipeDeletionSensor.position, pipeDeletionSensor.forward * 10, Color.green);
+        if (Physics.Raycast(pipeDeletionSensor.position, pipeDeletionSensor.forward, out hit, 10))
+        {
+            Debug.DrawRay(pipeDeletionSensor.position, pipeDeletionSensor.forward * 10, Color.red);
+            Destroy(hit.collider.gameObject.transform.parent.gameObject);
+        }
+    }
+    
+    public bool checkPipeCreationSensor()
+    {
+        RaycastHit hit;
+        Debug.DrawRay(pipeCreationSensor.position, pipeCreationSensor.forward * 10, Color.red);
+        if (!Physics.Raycast(pipeCreationSensor.position, pipeCreationSensor.forward, out hit, 10))
+        {
+            Debug.DrawRay(pipeCreationSensor.position, pipeCreationSensor.forward * 10, Color.green);
+            return true;
+        }
+
+        return false;
+    }
+    
     public void CreatePipe(PipeType type)
     {
         int rand = 0;
@@ -62,6 +97,7 @@ public class PipeCreator : MonoBehaviour
                 _currentGeneralPipeIndex = rand;
                 
                 tempPipe = Instantiate(generalPipe[rand], createLocation.position, createLocation.rotation, pipeHolder);
+                tempPipe.transform.localScale += new Vector3(0, pipeSizes[Random.Range(0, pipeSizes.Count)], 0);
                 break;
             case PipeType.Obstecle:
                 rand = Random.Range(0, obstreclePipe.Count);
