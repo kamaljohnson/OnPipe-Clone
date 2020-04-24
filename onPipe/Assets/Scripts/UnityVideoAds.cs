2f49d225262eb881ef9b607a1b66d6beb3992660
Kamal Johnson <kamal@kraker.in>
2020-04-24 10:04:27 +0530

    :sparkles: Add video ads

---
+++ onPipe/Assets/Scripts/UnityVideoAds.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Monetization;
+
+public class UnityVideoAds : MonoBehaviour {
+
+    public string placementId = "video";
+
+    public string gameId = "3568970";
+    public bool testMode = true;
+
+    public void Start()
+    {
+        Monetization.Initialize(gameId, testMode);
+        ShowAd();
+    }
+
+    public void ShowAd () {
+        StartCoroutine (ShowAdWhenReady ());
+    }
+
+    private IEnumerator ShowAdWhenReady () {
+        while (!Monetization.IsReady (placementId)) {
+            yield return new WaitForSeconds(0.25f);
+        }
+
+        ShowAdPlacementContent ad = null;
+        ad = Monetization.GetPlacementContent (placementId) as ShowAdPlacementContent;
+
+        if(ad != null) {
+            ad.Show ();
+        }
+    }
+}
